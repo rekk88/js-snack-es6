acf86667071fb4c0c7f65a5445a95853324e1bc6
maicol <maicol.matteoli@gmail.com>
2021-06-16 15:35:01 +0200

    test destructuring commentati

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-const studente ={
-    nome : "gianni",
-    cognome : "arancia",
-    eta : 20,
+// const studente ={
+//     nome : "gianni",
+//     cognome : "arancia",
+//     eta : 20,
 
-};
+// };
 
-console.log(studente);
-
-
-// studente.eta = 19;
-
-//destructuring
-let {nome , eta} = studente; //funziona sia con let che const
-
-console.log(nome , eta);
-
-nome = "gianni2";
+// console.log(studente);
 
 
-console.log(studente);
-console.log(nome);
+// // studente.eta = 19;
+
+// //destructuring
+// let {nome , eta} = studente; //funziona sia con let che const
+
+// console.log(nome , eta);
+
+// nome = "gianni2";
 
 
-const studenti = [studente , { nome : "gianna", cognome :"mandarino" , eta : 17}];
+// console.log(studente);
+// console.log(nome);
 
-console.log(studenti);
 
-const {newStudente} = studenti;
-console.log(newStudente);
+// const studenti = [studente , { nome : "gianna", cognome :"mandarino" , eta : 17}];
+
+// console.log(studenti);
+
+// const {newStudente} = studenti;
+// console.log(newStudente);
