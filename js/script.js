54c2d255a5b8c4babeb9c5dd5e28014940c41e18
maicol <maicol.matteoli@gmail.com>
2021-06-16 15:34:34 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const studente ={
+    nome : "gianni",
+    cognome : "arancia",
+    eta : 20,
+
+};
+
+console.log(studente);
+
+
+// studente.eta = 19;
+
+//destructuring
+let {nome , eta} = studente; //funziona sia con let che const
+
+console.log(nome , eta);
+
+nome = "gianni2";
+
+
+console.log(studente);
+console.log(nome);
+
+
+const studenti = [studente , { nome : "gianna", cognome :"mandarino" , eta : 17}];
+
+console.log(studenti);
+
+const {newStudente} = studenti;
+console.log(newStudente);
