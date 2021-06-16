959b152eda6279de709ce328e94715f791d442e7
maicol <maicol.matteoli@gmail.com>
2021-06-16 16:48:02 +0200

    fine es 2

--- js/script.js
+++ js/script.js
@@ -30,57 +30,112 @@
 // console.log(newStudente);
 
 //array di oggetti
-let bici = [
+
+
+
+
+
+//-----------------------------------es1------------------------------------------
+
+// let bici = [
+//     {
+//         nome:"mk1" ,
+//         peso: 20,
+//     },
+//     {
+//         nome: "mk2",
+//         peso: 23,
+//     },
+//     {
+//         nome: "mk22",
+//         peso: 1,
+//     },
+//     {
+//         nome: "mk45",
+//         peso: 29,
+//     },
+//     {
+//         nome: "mk0",
+//         peso: 100,
+//     },
+// ];
+
+// console.log(bici);
+// console.log(bici[2]);
+// // const {peso , nome} = bici[2];
+// // console.log(peso , nome);
+
+// let pesoMin = 999999;
+// let biciL;
+// for(let i=0 ; i < bici.length ; i++){
+
+//     if(bici[i].peso < pesoMin){
+//         pesoMin = bici[i].peso;
+//         biciL = i;
+//     }
+
+// }
+// // console.log(biciL);
+
+// //estraggo le proprietà dell'oggetto bici con peso minore
+// const{peso , nome} = bici[biciL];
+// console.log(peso , nome);
+
+
+// console.log(`La bici con peso minore è  ${nome}`);
+// document.getElementById("output").innerHTML = `
+// <ul>
+//     <li>La bici con peso minore è  ${nome}</li>
+// </ul>
+
+
+// `;
+
+//-----------------------------------es2------------------------------------------
+const rand = (min , max) => Math.floor(Math.random() * (max - min + 1) + min); //se metto le {} js si aspetta un return altrimenti senza le parentesi il return è implicito
+
+
+//caricamento array squadre
+let squadre = [
     {
-        nome:"mk1" ,
-        peso: 20,
+        nome : "mele",
+        punti : rand(0,100),
+        falli : rand(0,200),
     },
     {
-        nome: "mk2",
-        peso: 23,
+        nome : "fragole",
+        punti : rand(0,100),
+        falli : rand(0,200),
     },
     {
-        nome: "mk22",
-        peso: 1,
+        nome : "noci",
+        punti : rand(0,100),
+        falli : rand(0,200),
     },
     {
-        nome: "mk45",
-        peso: 29,
+        nome : "nani",
+        punti : rand(0,100),
+        falli : rand(0,200),
     },
     {
-        nome: "mk0",
-        peso: 100,
+        nome : "delfini",
+        punti : rand(0,100),
+        falli : rand(0,200),
     },
 ];
 
-console.log(bici);
-console.log(bici[2]);
-// const {peso , nome} = bici[2];
-// console.log(peso , nome);
+console.log(squadre);
+let squadra;
+const nomi = []; //nuovo array con nome e falli subiti
 
-let pesoMin = 999999;
-let biciL;
-for(let i=0 ; i < bici.length ; i++){
-
-    if(bici[i].peso < pesoMin){
-        pesoMin = bici[i].peso;
-        biciL = i;
+for(let i=0 ; i < squadre.length ; i++){
+    const {nome, falli} = squadre[i];
+    squadra = {
+        nome : nome,
+        falli : falli,
     }
+    console.log(squadra);
+    nomi.push(squadra);
 
 }
-// console.log(biciL);
-
-//estraggo le proprietà dell'oggetto bici con peso minore
-const{peso , nome} = bici[biciL];
-console.log(peso , nome);
-
-
-console.log(`La bici con peso minore è  ${nome}`);
-document.getElementById("output").innerHTML = `
-<ul>
-    <li>La bici con peso minore è  ${nome}</li>
-</ul>
-
-
-`;
-
+console.log(nomi);
